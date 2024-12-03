a44b720f5d0c09824bd8b773e74b43d3c4cbed4c
Powell, Madison Danielle <2021powell.madison@gmail.com>
2024-12-03 00:09:35 -0500

    Scales and Progress
    Added scaled for danceability and streams. Fixed CSV

--- Main_Viz/main.js
+++ Main_Viz/main.js
@@ -1,3 +1,24 @@
 d3.csv('Spotify_Youtube.csv').then(function(datum){
-    console.log(datum["Url_spotify"]);
+    // console.log(datum[0]["Url_spotify"]);
+
+    var maxStream = d3.max(datum, function(d){return +d['Stream'];});
+
+    var xScale = d3.scaleLinear().domain([0, 1]).range([50, 900]) // Dancebility Range
+    var yScale = d3.scaleLinear().domain([0, maxStream]).range([50, 900]) // Streams Range
+
+    var xAxis = d3.axisBottom(xScale).ticks(10);
+    var yAxis = d3.axisLeft(yScale);
+
+    var svg = d3.select('svg');
+    
+    svg.append('g')
+        .attr('class','x axis')
+        .attr('transform', 'translate(80, 900)')
+        .call(xAxis);
+
+    svg.append('g')
+        .attr('class','y axis')
+        .attr('transform', 'translate(100, 0)')
+        .call(yAxis);
+
 })
