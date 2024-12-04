26c55f761bb0d5b27679477aafb0c4f7503cb519
Powell, Madison Danielle <2021powell.madison@gmail.com>
2024-12-04 18:41:16 -0500

    Functional Drop Down!

--- Main_Viz/main.js
+++ Main_Viz/main.js
@@ -5,70 +5,6 @@ d3.csv('Spotify_Youtube.csv', function(datum){
         d.Stream = +d.Stream;
     });
 
-    var xLow = 0;
-    var xHigh = 1;
-    var xValue = "Danceability"
-
-    var xScale = d3.scaleLinear().domain([xLow, xHigh]).range([50, 1500])
-
-    // Button
-    var options = ["Danceability", "Energy", "Loudness", "Speechiness", "Acousticness", "Instrumentalness", "Liveness", "Tempo"]
-
-
-    var dropSelect = d3.select("#scatplot")
-        .append('select')
-
-    dropSelect.selectAll('myOptions')
-        .data(options)
-        .enter()
-        .append('option')
-        .text(function (d) {return d; })
-        .attr("value", function (d) {return d;})
-    
-    dropSelect.on("change", function(d) {
-
-        // recover the option that has been chosen
-        var selectedOption = d3.select(this).property("value")
-
-    
-        
-        // run the updateChart function with this selected option
-        updateChart(selectedOption)
-
-    })
-
-    function updateChart(selectedOption) {
-        if (selectedOption == "Tempo") {
-            xLow = 0
-            xHigh = 250
-            xValue = selectedOption
-        } else if (selectedOption == "Loudness") {
-            xLow = -50.0
-            xHigh = 1.0
-            xValue = selectedOption
-        } else {
-            xLow = 0
-            xHigh = 1.0
-            xValue = selectedOption
-        }
-
-        xScale = d3.scaleLinear().domain([xLow, xHigh]).range([50, 1500])
-        console.log(typeof selectedOption)
-        
-        scatplot
-            .selectAll('circle')
-            .data(filteredData)
-            .enter()
-            .append('circle')
-            .attr('cx', d => xScale(d[xValue]) + 70)
-            .attr('cy', d => yScale(d.Stream) - 5)
-            .attr('r', d => rScale(d.Views))
-            .style('stroke', 'black')
-            .on("mouseover", mouseover)
-            .on("mousemove", mousemove)
-            .on("mouseleave", mouseleave)
-    }
-
     /******************************************
     Filtering data because it was too large! (It crashed my browser...)
      - Allows for top 2 albums based on Spotify Streams per Artist
@@ -77,19 +13,17 @@ d3.csv('Spotify_Youtube.csv', function(datum){
     const groupArtists = d3.group(datum, d => d.Artist);
     const filteredData = Array.from(groupArtists, ([artist, albums]) =>
         albums
-            .sort((a, b) => b.Stream - a.Stream)
-            .slice(0,2)
+            .sort((a, b) => b.Stream - a.Stream)[0]
         ).flat();
 
 
-    console.log(filteredData);
-
+    var xValue = "Danceability"  
     var maxStream = d3.max(datum, function(d){return +d['Stream'];});
     var maxViews = d3.max(datum, function(d){return +d['Views'];});
 
     // var xScale = d3.scaleLinear().domain([xLow, xHigh]).range([50, 1500]) // Views Range
     var yScale = d3.scalePow().domain([maxStream, 1]).range([50, 700]).exponent(.4) // Streams Range
-
+    var xScale = d3.scaleLinear().domain([0.0, 1.0]).range([50, 1500])
     var rScale = d3.scaleSqrt().domain([0, maxViews]).range([2, 15]) //Radius Range
 
     var xAxis = d3.axisBottom(xScale).ticks(10);
@@ -118,9 +52,6 @@ d3.csv('Spotify_Youtube.csv', function(datum){
         .append('g')
         .attr('transform', 'translate(100, 0)')
         .call(yAxis)
-
-    
-    
     
     var tooltip = d3.select('#scatplot')
         .append("div")
@@ -143,7 +74,7 @@ d3.csv('Spotify_Youtube.csv', function(datum){
     var mousemove = function(d) {
         tooltip
               .html("Song: " + d.Track + "<br>Artist: " + d.Artist + "<br>Album: " + d.Album + "<br>Total Streams on Spotify: " + d.Stream + "<br>Total Views on Youtube: " + d.Views)
-              .style('left', (event.pageX + 10) + 'px') // Adjusted to follow cursor
+              .style('left', (event.pageX + 10) + 'px')
               .style('top', (event.pageY + 10) + 'px');
         console.log('hovering');
           }
@@ -156,7 +87,64 @@ d3.csv('Spotify_Youtube.csv', function(datum){
               .style("opacity", 0.8)
           }
     
+    function updateChart() {
+        var circles = scatplot.selectAll('circle')
+            .data(filteredData);
     
+            // Enter: Add new circles
+        circles.enter()
+            .append('circle')
+            .merge(circles) // Merge enter and update
+            .attr('cx', d => xScale(d[xValue]) + 70)
+            .attr('cy', d => yScale(d.Stream) - 5)
+            .attr('r', d => rScale(d.Views))
+            .style('stroke', 'black')
+            .style('opacity', .2)
+            .on("mouseover", mouseover)
+            .on("mousemove", mousemove)
+            .on("mouseleave", mouseleave)
+            .merge(circles)
+            .transition()
+            .duration(800)
+            .attr('cx', d => xScale(d[xValue]) + 70) // Smoothly update position
+            .attr('cy', d => yScale(d.Stream) - 5)
+            .attr('r', d => rScale(d.Views))
+            .style('opacity', 1);
+        
+        circles.exit()
+            .transition()
+            .duration(800)
+            .style('opacity', 0)
+            .remove();
+    
+    
+            // Exit: Remove old circles
+            circles.exit().remove();
+    }
+
+    updateChart();
+
+    // Button
+    var options = ["Danceability", "Energy", "Speechiness", "Acousticness", "Instrumentalness", "Liveness"]
+
+
+    var dropSelect = d3.select("#scatplot")
+        .append('select')
+
+    dropSelect.selectAll('myOptions')
+        .data(options)
+        .enter()
+        .append('option')
+        .text(function (d) {return d; })
+        .attr("value", function (d) {return d;})
+    
+    dropSelect.on("change", function() {
+        // Recover the selected option
+        xValue = d3.select(this).property("value");
+
+        // Update the chart
+        updateChart();
+    });
     
 
 })
