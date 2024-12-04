67cbb22d3d5f044626e937eadd4fb73cce6c731f
Powell, Madison Danielle <2021powell.madison@gmail.com>
2024-12-03 20:30:41 -0500

    Broken Tooltip
    Tooltip not fully working but base code is there.

--- Main_Viz/main.js
+++ Main_Viz/main.js
@@ -53,6 +53,42 @@ d3.csv('Spotify_Youtube.csv', function(datum){
 
     // Creating Dots
 
+    
+    
+    var tooltip = d3.select('#scatplot')
+        .append("div")
+        .style("opacity", 0)
+        .attr("class", "tooltip")
+        .style("background-color", "white")
+        .style("border", "solid")
+        .style("border-width", "2px")
+        .style("border-radius", "5px")
+        .style("padding", "5px")
+    
+    var mouseover = function(d) {
+            tooltip
+              .style("opacity", 1)
+            d3.select(this)
+              .style("stroke", "black")
+              .style("opacity", 1)
+          }
+    
+    var mousemove = function(d) {
+        tooltip
+              .html("The exact value of<br>this cell is: " + d.Artist)
+              .style('left', (event.pageX + 10) + 'px') // Adjusted to follow cursor
+              .style('top', (event.pageY + 10) + 'px');
+        console.log('hovering');
+          }
+    
+    var mouseleave = function(d) {
+            tooltip
+              .style("opacity", 0)
+            d3.select(this)
+              .style("stroke", "none")
+              .style("opacity", 0.8)
+          }
+    
     scatplot
         .selectAll('circle')
         .data(filteredData)
@@ -62,5 +98,8 @@ d3.csv('Spotify_Youtube.csv', function(datum){
         .attr('cy', d => yScale(d.Stream))
         .attr('r', d => rScale(d.Views))
         .style('stroke', 'black')
+        .on("mouseover", mouseover)
+        .on("mousemove", mousemove)
+        .on("mouseleave", mouseleave)
 
 })
