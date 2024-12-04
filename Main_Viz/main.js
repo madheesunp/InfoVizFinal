ba4d85923dd8369638d7a29ee4fa84c6f4b83a37
Powell, Madison Danielle <2021powell.madison@gmail.com>
2024-12-04 16:01:30 -0500

    Working Tooltip + Broken xAxis Transition
    Working on getting the x-axis and dots to transition when selected from a dropdown menu. Broken but making progress. Using this as a guide (https://d3-graph-gallery.com/graph/line_select.html)

--- Main_Viz/main.js
+++ Main_Viz/main.js
@@ -5,8 +5,73 @@ d3.csv('Spotify_Youtube.csv', function(datum){
         d.Stream = +d.Stream;
     });
 
+    var xLow = 0;
+    var xHigh = 1;
+    var xValue = "Danceability"
+
+    var xScale = d3.scaleLinear().domain([xLow, xHigh]).range([50, 1500])
+
+    // Button
+    var options = ["Danceability", "Energy", "Loudness", "Speechiness", "Acousticness", "Instrumentalness", "Liveness", "Tempo"]
+
+
+    var dropSelect = d3.select("#scatplot")
+        .append('select')
+
+    dropSelect.selectAll('myOptions')
+        .data(options)
+        .enter()
+        .append('option')
+        .text(function (d) {return d; })
+        .attr("value", function (d) {return d;})
+    
+    dropSelect.on("change", function(d) {
+
+        // recover the option that has been chosen
+        var selectedOption = d3.select(this).property("value")
+
+    
+        
+        // run the updateChart function with this selected option
+        updateChart(selectedOption)
+
+    })
+
+    function updateChart(selectedOption) {
+        if (selectedOption == "Tempo") {
+            xLow = 0
+            xHigh = 250
+            xValue = selectedOption
+        } else if (selectedOption == "Loudness") {
+            xLow = -50.0
+            xHigh = 1.0
+            xValue = selectedOption
+        } else {
+            xLow = 0
+            xHigh = 1.0
+            xValue = selectedOption
+        }
+
+        xScale = d3.scaleLinear().domain([xLow, xHigh]).range([50, 1500])
+        console.log(typeof selectedOption)
+        
+        scatplot
+            .selectAll('circle')
+            .data(filteredData)
+            .enter()
+            .append('circle')
+            .attr('cx', d => xScale(d[xValue]) + 70)
+            .attr('cy', d => yScale(d.Stream) - 5)
+            .attr('r', d => rScale(d.Views))
+            .style('stroke', 'black')
+            .on("mouseover", mouseover)
+            .on("mousemove", mousemove)
+            .on("mouseleave", mouseleave)
+    }
+
     /******************************************
     Filtering data because it was too large! (It crashed my browser...)
+     - Allows for top 2 albums based on Spotify Streams per Artist
     ******************************************/
 
     const groupArtists = d3.group(datum, d => d.Artist);
@@ -22,14 +87,17 @@ d3.csv('Spotify_Youtube.csv', function(datum){
     var maxStream = d3.max(datum, function(d){return +d['Stream'];});
     var maxViews = d3.max(datum, function(d){return +d['Views'];});
 
-    var xScale = d3.scaleLinear().domain([0, 1]).range([50, 1500]) // Dancebility Range
-    var yScale = d3.scaleLinear().domain([maxStream, 0]).range([50, 700]) // Streams Range
-    var rScale = d3.scaleSqrt().domain([0, maxViews]).range([.5, 15]) //Radius Range
+    // var xScale = d3.scaleLinear().domain([xLow, xHigh]).range([50, 1500]) // Views Range
+    var yScale = d3.scalePow().domain([maxStream, 1]).range([50, 700]).exponent(.4) // Streams Range
+
+    var rScale = d3.scaleSqrt().domain([0, maxViews]).range([2, 15]) //Radius Range
 
     var xAxis = d3.axisBottom(xScale).ticks(10);
     var yAxis = d3.axisLeft(yScale);
 
     var svg = d3.select('svg');
+
+    
 
     // Scatterplot Setup
 
@@ -51,8 +119,7 @@ d3.csv('Spotify_Youtube.csv', function(datum){
         .attr('transform', 'translate(100, 0)')
         .call(yAxis)
 
-    // Creating Dots
-
+    
     
     
     var tooltip = d3.select('#scatplot')
@@ -75,7 +142,7 @@ d3.csv('Spotify_Youtube.csv', function(datum){
     
     var mousemove = function(d) {
         tooltip
-              .html("The exact value of<br>this cell is: " + d.Artist)
+              .html("Song: " + d.Track + "<br>Artist: " + d.Artist + "<br>Album: " + d.Album + "<br>Total Streams on Spotify: " + d.Stream + "<br>Total Views on Youtube: " + d.Views)
               .style('left', (event.pageX + 10) + 'px') // Adjusted to follow cursor
               .style('top', (event.pageY + 10) + 'px');
         console.log('hovering');
@@ -89,17 +156,7 @@ d3.csv('Spotify_Youtube.csv', function(datum){
               .style("opacity", 0.8)
           }
     
-    scatplot
-        .selectAll('circle')
-        .data(filteredData)
-        .enter()
-        .append('circle')
-        .attr('cx', d => xScale(d.Danceability))
-        .attr('cy', d => yScale(d.Stream))
-        .attr('r', d => rScale(d.Views))
-        .style('stroke', 'black')
-        .on("mouseover", mouseover)
-        .on("mousemove", mousemove)
-        .on("mouseleave", mouseleave)
+    
+    
 
 })
