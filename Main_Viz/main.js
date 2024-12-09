10016b7b56aadf55d4d8c2d255145548acbe605c
Powell, Madison Danielle <2021powell.madison@gmail.com>
2024-12-09 11:34:39 -0500

    Final Changes

--- Main_Viz/main.js
+++ Main_Viz/main.js
@@ -1,10 +1,20 @@
 d3.csv('Spotify_Youtube.csv', function(datum){
 
+    // Making sure that data values are numerical!
 
     datum.forEach(d => {
         d.Stream = +d.Stream;
+        d.Views = +d.Views;
+        d.Danceability = +d.Danceability;
+        d.Energy = +d.Energy;
+        d.Speechiness = +d.Speechiness;
+        d.Acousticness = +d.Acousticness;
+        d.Instrumentalness = +d.Instrumentalness;
+        d.Liveness = +d.Liveness;
     });
 
+    
+    
     /******************************************
     Filtering data because it was too large! (It crashed my browser...)
      - Allows for top 2 albums based on Spotify Streams per Artist
@@ -16,15 +26,21 @@ d3.csv('Spotify_Youtube.csv', function(datum){
             .sort((a, b) => b.Stream - a.Stream)[0]
         ).flat();
 
+    console.log(filteredData);
 
-    var xValue = "Danceability"  
+    var xValue = "Danceability"
+    let currentPoint = datum[0]; // Variable for selected data
+
+     /******************************************
+      * Setting up Axis
+    ******************************************/
+    
     var maxStream = d3.max(datum, function(d){return +d['Stream'];});
     var maxViews = d3.max(datum, function(d){return +d['Views'];});
 
-    // var xScale = d3.scaleLinear().domain([xLow, xHigh]).range([50, 1500]) // Views Range
-    var yScale = d3.scalePow().domain([maxStream, 1]).range([50, 700]).exponent(.4) // Streams Range
-    var xScale = d3.scaleLinear().domain([0.0, 1.0]).range([50, 1500])
-    var rScale = d3.scaleSqrt().domain([0, maxViews]).range([2, 15]) //Radius Range
+    var yScale = d3.scaleLinear().domain([maxStream, 1]).range([50, 700])// Streams Range
+    var xScale = d3.scaleLinear().domain([0.0, 1.0]).range([50, 1300]) // Option Scale
+    var rScale = d3.scalePow().domain([0, maxViews]).range([2, 15]).exponent(.4) //Radius Range
 
     var xAxis = d3.axisBottom(xScale).ticks(10);
     var yAxis = d3.axisLeft(yScale);
@@ -39,7 +55,7 @@ d3.csv('Spotify_Youtube.csv', function(datum){
         .select("#scatplot")
         .append("svg:svg")
         .attr("id", "svg1")
-        .attr("width", 1600)
+        .attr("width", 1400)
         .attr("height", 800)
         .attr("stroke", 10)
     
@@ -52,6 +68,15 @@ d3.csv('Spotify_Youtube.csv', function(datum){
         .append('g')
         .attr('transform', 'translate(100, 0)')
         .call(yAxis)
+
+    scatplot.append("text")
+        .attr("class", "y-axis-label")
+        .attr("text-anchor", "middle")
+        .attr("transform", "rotate(-90)")
+        .attr("x", -800 / 2)
+        .attr("y", 15)
+        .text("Stream Count");
+    
     
     var tooltip = d3.select('#scatplot')
         .append("div")
@@ -64,62 +89,67 @@ d3.csv('Spotify_Youtube.csv', function(datum){
         .style("padding", "5px")
     
     var mouseover = function(d) {
+        currentPoint = d;
             tooltip
               .style("opacity", 1)
             d3.select(this)
               .style("stroke", "black")
+              .style("stroke-width", 2)
               .style("opacity", 1)
-          }
+        updateBarplot(currentPoint);
+    }
     
     var mousemove = function(d) {
         tooltip
               .html("Song: " + d.Track + "<br>Artist: " + d.Artist + "<br>Album: " + d.Album + "<br>Total Streams on Spotify: " + d.Stream + "<br>Total Views on Youtube: " + d.Views)
               .style('left', (event.pageX + 10) + 'px')
               .style('top', (event.pageY + 10) + 'px');
-        console.log('hovering');
-          }
+
+        console.log(currentPoint)
+    }
     
     var mouseleave = function(d) {
             tooltip
               .style("opacity", 0)
             d3.select(this)
               .style("stroke", "none")
-              .style("opacity", 0.8)
-          }
+              .style("opacity", 1)
+    }
+
+     /******************************************
+      * Updates Scatter Plot
+    ******************************************/
     
     function updateChart() {
         var circles = scatplot.selectAll('circle')
             .data(filteredData);
     
-            // Enter: Add new circles
         circles.enter()
             .append('circle')
-            .merge(circles) // Merge enter and update
+            .merge(circles)
+            .attr("class", "circ")
             .attr('cx', d => xScale(d[xValue]) + 70)
             .attr('cy', d => yScale(d.Stream) - 5)
             .attr('r', d => rScale(d.Views))
-            .style('stroke', 'black')
-            .style('opacity', .2)
+            .style('opacity', .8)
+            .style('stroke', 'none')
             .on("mouseover", mouseover)
             .on("mousemove", mousemove)
             .on("mouseleave", mouseleave)
             .merge(circles)
             .transition()
-            .duration(800)
-            .attr('cx', d => xScale(d[xValue]) + 70) // Smoothly update position
+            .duration(1600)
+            .attr('cx', d => xScale(d[xValue]) + 70)
             .attr('cy', d => yScale(d.Stream) - 5)
-            .attr('r', d => rScale(d.Views))
-            .style('opacity', 1);
+            .attr('r', d => rScale(d.Views));
         
         circles.exit()
             .transition()
-            .duration(800)
+            .duration(1600)
             .style('opacity', 0)
             .remove();
     
-    
-            // Exit: Remove old circles
-            circles.exit().remove();
+
     }
 
     updateChart();
@@ -130,6 +160,7 @@ d3.csv('Spotify_Youtube.csv', function(datum){
 
     var dropSelect = d3.select("#scatplot")
         .append('select')
+        .attr('id', 'dropdown')
 
     dropSelect.selectAll('myOptions')
         .data(options)
@@ -139,12 +170,96 @@ d3.csv('Spotify_Youtube.csv', function(datum){
         .attr("value", function (d) {return d;})
     
     dropSelect.on("change", function() {
-        // Recover the selected option
         xValue = d3.select(this).property("value");
 
-        // Update the chart
         updateChart();
     });
+
+
+    const formattedData = options.map(option => ({
+        option: option,
+        value: +currentPoint[option]
+    }));
     
+    var x2Scale = d3.scaleBand().range([50,450]).padding(0.2)
+    var y2Scale = d3.scaleLinear().domain([1.0, 0.0]).range([50, 450])
+
+    var x2Axis = d3.axisBottom(x2Scale);
+    var y2Axis = d3.axisLeft(y2Scale);
+
+    var barplot = d3
+        .select('#barplot')
+        .append("svg:svg")
+        .attr("id", "svg2")
+        .attr("width", 500)
+        .attr("height", 500)
+
+    barplot.append('g')
+        .attr('class', 'x-axis')
+        .attr('transform', 'translate(10, 450)')
+        .call(x2Axis)
+    
+    barplot.append('g')
+        .attr('class', 'y-axis')
+        .attr('transform', 'translate(50, 0)')
+        .call(y2Axis)
+    
+    /******************************************
+      * Updates Bar Plot
+    ******************************************/
+
+    function updateBarplot(dataPoint) {
+        const updatedData = options.map(option => ({
+            option: option,
+            value: +dataPoint[option]
+        }));
+
+        x2Scale.domain(updatedData.map (d => d.option));
+
+        barplot.select(".x-axis")
+            .call(x2Axis)
+            .selectAll("text")
+            .attr("transform", "rotate(-45)")
+            .style("text-anchor", "end");
+        
+        const bars = barplot.selectAll('rect').data(updatedData);
+
+        bars.enter()
+            .append("rect")
+            .merge(bars)
+            .transition()
+            .duration(500)
+            .attr('x', d => x2Scale(d.option) + 10)
+            .attr("class", "bars")
+            .attr('y', d => y2Scale(d.value))
+            .attr('width', x2Scale.bandwidth())
+            .attr('height', d => 450 - y2Scale(d.value))
+            .attr('fill', '#69b3a2');
+
+        // Exit
+        bars.exit()
+            .transition()
+            .duration(500)
+            .attr("height", 0)
+            .remove();
+        
+        var textDesc = barplot.selectAll(".description-text")
+            .data([dataPoint]);
+        
+        textDesc.enter()
+            .append("text")
+            .attr("class", "description-text")
+            .attr("text-anchor", "left")
+            .attr("x", 10)
+            .attr("y", 20) // Position the text appropriately
+            .merge(textDesc) // Update existing element
+            .text(d => "You're currently stats for: " + d.Track + " by " + d.Artist);
+
+    }
+
+    updateBarplot(currentPoint);
+
+
+
 
 })
